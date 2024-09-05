099dfc57fe7eacb1d4da36626c9030e4138a25a7
victoraugustosouza <victor_engenharia@poli.ufrj.br>
2024-09-05 13:30:18 -0300

    Update selenium-template.py

--- selenium-template.py
+++ selenium-template.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from selenium.webdriver.support.ui import Select
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
+from datetime import datetime
+import os
+import unicodedata
+import pandas as pd
 display = Display(visible=0, size=(800, 800))  
 display.start()
 
