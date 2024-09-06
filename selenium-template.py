d951a21de89903bcb96b9f9c053f01abc4622900
victoraugustosouza <victor_engenharia@poli.ufrj.br>
2024-09-06 07:42:29 -0300

    Update selenium-template.py

--- selenium-template.py
+++ selenium-template.py
@@ -55,3 +55,9 @@ login_button.click()
 
 time.sleep(5)
 print("Login realizado.")
+
+while driver.current_url != "https://associado.appai.org.br/":
+    pass
+
+print("Carregando página de eventos...")
+driver.get("https://associado.appai.org.br/bom-espetaculo")
